b7665ab75306a76fba20104cae73ac5a9c759e54
sullivanpj <pat@patsullivan.org>
2026-06-28 19:29:32 +0000

    feat(hey-api-output): Initial check-in of the Hey API output package

---
+++ packages/outputs/hey-api-output/src/index.ts
@@ -0,0 +1,76 @@
+/* -------------------------------------------------------------------
+
+                  🗲 Storm Software - Power Plant
+
+ This code was released as part of the Power Plant project. Power Plant
+ is maintained by Storm Software under the Apache-2.0 license, and is
+ free for commercial and private use. For more information, please visit
+ our licensing page at https://stormsoftware.com/licenses/projects/power-plant.
+
+ Website:                  https://stormsoftware.com
+ Repository:               https://github.com/storm-software/power-plant
+ Documentation:            https://docs.stormsoftware.com/projects/power-plant
+ Contact:                  https://stormsoftware.com/contact
+
+ SPDX-License-Identifier:  Apache-2.0
+
+ ------------------------------------------------------------------- */
+
+import type { UserConfig } from "@hey-api/openapi-ts";
+import { createClient } from "@hey-api/openapi-ts";
+import { useContext } from "@power-plant/core";
+import { defineOutput } from "@power-plant/core/output/define";
+import type { OpenAPISchema } from "@power-plant/openapi-schema";
+import openapiSchema from "@power-plant/openapi-schema";
+import { toArray } from "@stryke/convert/to-array";
+import type { Arrayable } from "@stryke/types/array";
+import packageJson from "../package.json" with { type: "json" };
+
+const output = defineOutput<OpenAPISchema, Arrayable<UserConfig>>({
+  meta: {
+    name: "hey-api",
+    title: "Hey API",
+    description:
+      "A output that uses the OpenAPI specification to generate source code using Hey API.",
+    version: packageJson.version,
+    tags: ["openapi"],
+    links: [
+      {
+        href: "https://heyapi.dev",
+        description: "Hey API Documentation"
+      },
+      {
+        href: "https://github.com/heyapi/heyapi",
+        description: "Hey API GitHub Repository"
+      }
+    ]
+  },
+  schema: openapiSchema,
+  output: async (spec, options) => {
+    const { fs } = useContext();
+
+    const context = await createClient(
+      toArray(options).map(option => ({
+        ...option,
+        input: spec,
+        output: {
+          path: "src/api"
+        }
+      }))
+    );
+
+    await Promise.all(
+      context.map(async c =>
+        Promise.all(
+          c.gen
+            .render()
+            .map(async r =>
+              fs.promises.writeFile(r.path, r.content, { encoding: "utf8" })
+            )
+        )
+      )
+    );
+  }
+});
+
+export default output;
---
+++ packages/outputs/hey-api-output/vitest.config.mts
@@ -0,0 +1,21 @@
+import { nxCopyAssetsPlugin } from "@nx/vite/plugins/nx-copy-assets.plugin";
+import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig(() => ({
+  root: __dirname,
+  cacheDir: "../../../node_modules/.vite/packages/outputs/hey-api-output",
+  plugins: [nxViteTsPaths(), nxCopyAssetsPlugin(["*.md"])],
+  test: {
+    name: "hey-api-output",
+    watch: false,
+    globals: true,
+    environment: "node",
+    include: ["{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    reporters: ["default"],
+    coverage: {
+      reportsDirectory: "../../../coverage/packages/outputs/hey-api-output",
+      provider: "v8" as const
+    }
+  }
+}));
